956e1a9ebfcc8c86bcad6ca0f7a47745f2b063b9
kevinzin030 <160799494+kevinzin030@users.noreply.github.com>
2026-04-22 18:30:28 -0300

    Delete site teste directory

--- site teste/app.js
+++
@@ -1,9 +0,0 @@
-function init(){
-  verificarLogin();
-
-  let user = JSON.parse(localStorage.getItem("logado"));
-
-  if(user.tipo === "aluno") carregarAluno(user);
-  if(user.tipo === "gestor") carregarGestor();
-  if(user.tipo === "admin") carregarAdmin();
-}
